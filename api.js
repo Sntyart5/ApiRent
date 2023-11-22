5b711393c693570f8d03434c242a9c3e7a7af4c4
Santiago Vargas <santyvara05@gmail.com>
2023-11-22 08:20:30 -0500

    se configura cors

--- api.js
+++ api.js
@@ -18,9 +18,22 @@ WakeServer(){
 ConectarDB(){
     Conexion()
 }
-processPetition(){
-    this.app.use(cors())
-this.app.use(express.json())
-this.app.use("/",Routes)
+processPetition() {
+    // Configurar CORS solo para las rutas específicas que necesitas
+    const corsOptions = {
+        origin: 'http://localhost:19006', // Reemplaza esto con la URL de tu aplicación cliente
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+        optionsSuccessStatus: 204,
+    };
+
+    this.app.use('/obtener', cors(corsOptions));
+    this.app.use('/actualizar', cors(corsOptions));
+    this.app.use('/eliminar', cors(corsOptions));
+    this.app.use('/mandar', cors(corsOptions));
+
+    this.app.use(express.json());
+    this.app.use("/", Routes);
 }
+
 }
